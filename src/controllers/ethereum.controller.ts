7b0007a6ecbd08115446ce43be0d461beb624ecf
mohamad-khaled <mohamed.khaled@rackspace.com>
2022-12-05 23:15:20 +0200

    add status codes to responses

--- src/controllers/ethereum.controller.ts
+++ src/controllers/ethereum.controller.ts
@@ -9,17 +9,22 @@ export class EthereumController {
 
     @GET()
     async getTotalBalance(req: Request, res: Response) {
-        //check if addresses only up to 100
-        if (req.query.address !== undefined) {
-            const addresses = validate(req.query.address.toString());
-            if (addresses) {
-                const balances = await this.ethereumService.getTotalBalance(addresses)
-                console.log("balance", balances)
-                return res.json(balances);
-            } else {
-                return res.json({ mssg: 'you have excceded addresses limit of 100' });
+        try {
+            //check if addresses only up to 100
+            if (req.query.address !== undefined && req.query.address.toString() !== "") {
+                const addresses = validate(req.query.address.toString());
+                if (addresses) {
+                    const balances = await this.ethereumService.getTotalBalance(addresses)
+                    console.log("addresses and their balance and total balance", balances)
+                    return res.json(balances);
+                } else {
+                    return res.json({ mssg: 'you have excceded addresses limit of 100' });
+                }
             }
+            return res.status(400).json({ mssg: 'please enter addresses' });
+        } catch (error) {
+            console.log("error msg", error)
+            return res.status(500).json({ mssg: 'internal server error' });
         }
-        return res.json({ mssg: 'please enter addresses' });
     }
 }
